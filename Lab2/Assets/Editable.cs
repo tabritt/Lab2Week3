f57be14d2e73f0e66a7be53d27c6ec7049963e81
Tiger <135745508+tabritt@users.noreply.github.com>
2025-09-07 23:40:25 -0400

    pushhh

--- Lab2/Assets/Editable.cs
+++ Lab2/Assets/Editable.cs
@@ -1,3 +1,11 @@
 using UnityEngine;
 
-public class Editable : MonoBehaviour { }
+public class Editable : MonoBehaviour
+{
+    [Header("Shape Sizes")]
+    [Tooltip("Controls the size of all cubes with this tag.")]
+    public float cubeSize = 1f;
+
+    [Tooltip("Controls the radius of all spheres with this tag.")]
+    public float sphereRadius = 1f;
+}
--- Lab2/Assets/Editor/Editable.cs
+++ Lab2/Assets/Editor/Editable.cs
@@ -2,15 +2,54 @@ using UnityEngine;
 using UnityEditor;
 using System.Linq;
 
-[CustomEditor(typeof(Editable)), CanEditMultipleObjects] //allows for editing of multiple objects
+[CustomEditor(typeof(Editable))]
+[CanEditMultipleObjects] // enables multi-object editing
 public class EditableEditor : Editor
 {
+    SerializedProperty cubeSize;
+    SerializedProperty sphereRadius;
+
+    private void OnEnable()
+    {
+        // Links the Serialized properties from Editable Ediotr Script
+        cubeSize = serializedObject.FindProperty("cubeSize");
+        sphereRadius = serializedObject.FindProperty("sphereRadius");
+    }
+
     public override void OnInspectorGUI()
     {
+        serializedObject.Update();
 
+        // Draw default inspector values first
         base.OnInspectorGUI();
 
-        EditorGUILayout.BeginHorizontal(); //begins wrap for horizontal button layout
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Shape Size Settings", EditorStyles.boldLabel);
+
+        // Cube size field + warnings
+        EditorGUILayout.PropertyField(cubeSize, new GUIContent("Cube Size"));
+        if (cubeSize.floatValue > 2f)
+        {
+            EditorGUILayout.HelpBox(" The cubes' sizes cannot be bigger than 2!", MessageType.Warning);
+        }
+        else if (cubeSize.floatValue <= 0f)
+        {
+            EditorGUILayout.HelpBox(" Cube has to be > 0 for sure!!!!", MessageType.Error);
+        }
+
+        // Sphere radius field + warnings
+        EditorGUILayout.PropertyField(sphereRadius, new GUIContent("Sphere Radius"));
+        if (sphereRadius.floatValue < 1f)
+        {
+            EditorGUILayout.HelpBox(" The spheres' radius cannot be smaller than 1!", MessageType.Warning);
+        }
+        else if (sphereRadius.floatValue > 5f)
+        {
+            EditorGUILayout.HelpBox(" Sphere too big?!!", MessageType.Info);
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Shape Selection", EditorStyles.boldLabel);
 
         if (GUILayout.Button("Select all Cubes")) //swapped enimes to the shapes
         {
@@ -21,8 +60,6 @@ public class EditableEditor : Editor
         {
             SelectTag("Sphere"); //different font
         }
-        
-        EditorGUILayout.EndHorizontal(); // wrap for horizontal button layout
 
         if (GUILayout.Button("Clear Selection")) //removes the selection
         {
@@ -31,6 +68,8 @@ public class EditableEditor : Editor
 
         DrawToggle("Cube");
         DrawToggle("Sphere");
+
+        serializedObject.ApplyModifiedProperties();
     }
 
     void SelectTag(string tag) //adds the tagged objects in an array
@@ -42,25 +81,43 @@ public class EditableEditor : Editor
     void DrawToggle(string tag) // this function is just saying that if this tag exists in the scene then this button will exists
     {
         var objs = GameObject.FindGameObjectsWithTag(tag).ToList();
+        if (objs.Count == 0)
+            return; //exit method if 0
 
-        //if (objs.Count == 0)
-          //  return; //exit method if 0
+        bool allVisible = objs.All(o => o.activeSelf && !SceneVisibilityManager.instance.IsHidden(o));
+        string buttonText = allVisible ? $"Hide all {tag}s" : $"Show all {tag}s"; //this button specifically
 
-        bool allActive = objs.All(o => o.activeSelf);
-
-        string buttonText = allActive ? $"Hide all {tag}s" : $"Show all {tag}s"; //this button specifically
-
-        GUI.color = allActive ? Color.green : Color.yellow;
+        GUI.color = allVisible ? Color.green : Color.yellow;
 
         if (GUILayout.Button(buttonText))
         {
+            Undo.SetCurrentGroupName(buttonText);
+            int group = Undo.GetCurrentGroup();
+
             foreach (var obj in objs)
             {
-                obj.SetActive(!allActive);
+                UndoHiddenObjs(obj, buttonText);
+
+                // Toggle active state
+                obj.SetActive(!allVisible);
+
+                // Toggle Scene visibility in the Editor
+                if (allVisible)
+                    SceneVisibilityManager.instance.Hide(obj, false);
+                else
+                    SceneVisibilityManager.instance.Show(obj, false);
+
+                EditorUtility.SetDirty(obj);
             }
+
+            Undo.CollapseUndoOperations(group);
         }
 
         GUI.color = Color.white;
     }
 
+    void UndoHiddenObjs(GameObject obj, string actionName)
+    {
+        Undo.RegisterFullObjectHierarchyUndo(obj, actionName);
+    }
 }
